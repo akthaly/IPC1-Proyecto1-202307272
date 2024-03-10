566d6c581d055e68e3479ed98b07d78e346cd234
harukixzh <bryan2014ap@gmail.com>
2024-03-09 20:41:34 -0600

    FINAL

--- Proyecto1/src/main/java/org/example/Main.java
+++ Proyecto1/src/main/java/org/example/Main.java
@@ -5,9 +5,6 @@ import interfaz.Login;
 
 import javax.swing.*;
 
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static Administrador administrador = new Administrador("admin", "admin", "admin");
